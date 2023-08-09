caec9ada588cb2bbf676df85d80a5be8ac5c5767
LHC <124650324+1941289645@users.noreply.github.com>
2023-08-09 20:42:40 +0800

    添加axios响应拦截器
     401 代表身份验证失败

--- utils/request.js
+++ utils/request.js
@@ -11,3 +11,19 @@ axios.interceptors.request.use(function (config){
 }),function (error){
     return Promise.reject(error);
 }
+
+
+//添加响应拦截器
+axios.interceptors.response.use(function (response) {
+    // 2xx 范围内的状态码都会触发该函数。
+    return response;
+  }, function (error) {
+    // 超出 2xx 范围的状态码都会触发该函数。
+    // 对响应错误做点什么，例如：判断响应状态为 401 代表身份验证失败
+    if (error?.response?.status === 401) {
+      alert('登录状态过期，请重新登录')
+      localStorage.clear()
+      window.location.href = '../login/index.html'
+    }
+    return Promise.reject(error);
+  });
