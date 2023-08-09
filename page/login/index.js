da1cccc1bfb013c9f7c11bb3bccbf5dcdb503917
LHC <124650324+1941289645@users.noreply.github.com>
2023-08-09 17:57:46 +0800

    验证码登录
    axios 公共配置，插件收集数据，调用接口，弹出弹框

--- page/login/index.js
+++ page/login/index.js
@@ -5,3 +5,23 @@
  * 1.3 基于 axios 调用验证码登录接口
  * 1.4 使用 Bootstrap 的 Alert 警告框反馈结果给用户
  */
+
+//1.2 收集手机号和验证码数据
+document.querySelector('.btn').addEventListener('click',()=>{
+     const form=document.querySelector('.login-form')
+     const data=serialize(form,{hash:true,empty:true})
+     console.log(data)
+
+    //1.3 基于 axios 调用验证码登录接口
+    axios({
+        url:'/v1_0/authorizations',
+        method:'POST',
+        data
+    }).then(res=>{
+        myAlert(true,'登录成功')
+        console.log(res);
+    }).catch(err=>{
+        myAlert(false,err.response.data.message)
+        console.dir(err.response.data.message);
+    })
+})
